6fc67ade88852e942b2fd98a130ef689811e2404
Bo Guan <neu.guanbo@gmail.com>
2021-02-14 13:34:10 -0500

    Update algorithm1_thoery_l1.m

--- algorithm1_thoery_l1.m
+++ algorithm1_thoery_l1.m
@@ -2,11 +2,11 @@ function [Pro_Bu] = algorithm1_thoery_l1(p_obf, r, m)
 % lower bound for the algorithm1 obfuscation when l = 1
 
 Pro_Bu = 0;
-for k = 0:r-2
-    Pro_Bu = Pro_Bu + nchoosek(m, k)*p_obf^k*(1-p_obf)^(m-k)*(k/(r-1));
+for k = 0:r-1
+    Pro_Bu = Pro_Bu + nchoosek(m, k)*p_obf^k*(1-p_obf)^(m-k)*(k/r);
     
 end
-for k = (r-1):m
+for k = r:m
     Pro_Bu = Pro_Bu + nchoosek(m, k)*p_obf^k*(1-p_obf)^(m-k);
 end
 end
